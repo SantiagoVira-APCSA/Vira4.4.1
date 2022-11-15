c5d7afdec3fb9cce61b9d43fdfee00ad445e34a5
SantiagoVira <santiagovira06@gmail.com>
2022-11-15 12:06:00 -0500

    Code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int result = 0;
+        int m = 0;
+        while (m < 3) {
+            int k = 0;
+            while (k < 4) {
+                result++;
+                k++;
+                System.out.print("k: " + k);
+                System.out.println(", m: " + m);
+            }
+            m++;
+        }
+        System.out.println("result: " + result);
+
+
     }
 }
